e992fcba5a3f8f8b04e14633a61c8f6a2d6f67d0
Yinlin Li <yinlin@seal.io>
2024-03-09 15:11:42 +0800

    del: role assignment

--- main.tf
+++ main.tf
@@ -45,15 +45,3 @@ data "azurerm_public_ip" "pubip" {
   name                = reverse(split("/", tolist(azurerm_kubernetes_cluster.aks.network_profile.0.load_balancer_profile.0.effective_outbound_ips)[0]))[0]
   resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
 }
-
-resource "azurerm_role_assignment" "aks_cluster_admin" {
-  scope                = azurerm_kubernetes_cluster.aks.id
-  role_definition_name = "Azure Kubernetes Service Cluster Admin Role"
-  principal_id         = var.principal_id
-}
-
-resource "azurerm_role_assignment" "aks_cluster_user" {
-  scope                = azurerm_kubernetes_cluster.aks.id
-  role_definition_name = "Azure Kubernetes Service Cluster User Role"
-  principal_id         = var.principal_id
-}
